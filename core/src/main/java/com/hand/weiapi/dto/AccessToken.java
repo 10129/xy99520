f89bf81b655137caf021010e1d4072ad51eaa29f
10129 <shuai.xie@hand-china.com>
2018-01-17 23:11:33 +0800

    微信API

---
+++ core/src/main/java/com/hand/weiapi/dto/AccessToken.java
@@ -0,0 +1,21 @@
+package com.hand.weiapi.dto;
+
+/**
+ * Created by xieshuai on 2018/1/16.
+ */
+public class AccessToken {
+    private String tokenName; //获取到的凭证
+    private int expireSecond; //凭证有效时间 单位:秒
+    public String getTokenName() {
+        return tokenName;
+    }
+    public void setTokenName(String tokenName) {
+        this.tokenName = tokenName;
+    }
+    public int getExpireSecond() {
+        return expireSecond;
+    }
+    public void setExpireSecond(int expireSecond) {
+        this.expireSecond = expireSecond;
+    }
+}
---
+++ core/src/main/java/com/hand/weiapi/dto/AccessTokenInfo.java
@@ -0,0 +1,8 @@
+package com.hand.weiapi.dto;
+
+/**
+ * Created by xieshuai on 2018/1/16.
+ */
+public class AccessTokenInfo {
+    public static AccessToken accessToken = null;
+}
